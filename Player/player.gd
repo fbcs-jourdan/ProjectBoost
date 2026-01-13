f6358162be7cab3c4b4fa3642cab456cc64b64b9
fbcs-jourdan <2260504@flourbluffschools.org>
2026-01-13 12:09:23 -0600

    added particles

--- Player/player.gd
+++ Player/player.gd
@@ -6,6 +6,9 @@ class_name Player
 @onready var death: AudioStreamPlayer = $death
 @onready var success: AudioStreamPlayer = $success
 @onready var rocket_audio: AudioStreamPlayer3D = $RocketAudio
+@onready var main_booster: GPUParticles3D = $MainBooster
+@onready var right_booster: GPUParticles3D = $RightBooster
+@onready var left_booster: GPUParticles3D = $LeftBooster
 
 var transitioning := false
 
@@ -16,15 +19,23 @@ func _process(delta: float) -> void:
 	if not transitioning:
 		if Input.is_action_pressed("boost"):
 			apply_central_force(basis.y * delta * thrust)
+			main_booster.emitting = true
 			if not rocket_audio.is_playing():
 				rocket_audio.play()
 		else:
 			rocket_audio.stop()
+			main_booster.emitting = false
 			
 		if Input.is_action_pressed("rotate_right"):
 			apply_torque(Vector3(0, 0, -delta * torque_thrust))
+			left_booster.emitting = true
+		else:
+			left_booster.emitting = false
 		if Input.is_action_pressed("rotate_left"):
 			apply_torque(Vector3(0, 0, delta * 100))
+			right_booster.emitting = true
+		else:
+			right_booster.emitting = false
 
 func _on_body_entered(body: Node) -> void:
 	if not transitioning:
@@ -43,6 +54,10 @@ func _on_body_entered(body: Node) -> void:
 func crash_sequence():
 	transitioning = true
 	print("KABOOM")
+	rocket_audio.stop()
+	main_booster.emitting = false
+	right_booster.emitting = false
+	left_booster.emitting = false
 	death.play()
 	await get_tree().create_timer(1).timeout
 	get_tree().reload_current_scene.call_deferred()
@@ -50,5 +65,9 @@ func crash_sequence():
 func complete_level(next_level_file):
 	success.play()
 	transitioning = true
+	rocket_audio.stop()
+	main_booster.emitting = false
+	right_booster.emitting = false
+	left_booster.emitting = false
 	await get_tree().create_timer(1).timeout
 	get_tree().change_scene_to_file.call_deferred(next_level_file)
