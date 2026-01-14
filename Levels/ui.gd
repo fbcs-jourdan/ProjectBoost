0192864f70af4378eef665c092d32a5e626371b0
fbcs-jourdan <2260504@flourbluffschools.org>
2026-01-14 13:09:17 -0600

    final level + ui added

---
+++ Levels/ui.gd
@@ -0,0 +1,6 @@
+extends CanvasLayer
+
+@onready var label: Label = $MarginContainer/CenterContainer/Label
+
+func update_fuel(amt):
+	label.text = "Fuel: " + str(amt)
--- Player/player.gd
+++ Player/player.gd
@@ -3,6 +3,7 @@ class_name Player
 
 @export_range(750, 2500) var thrust := 1000.0
 @export var torque_thrust := 100
+@export var starting_fuel := 100
 @onready var death: AudioStreamPlayer = $death
 @onready var success: AudioStreamPlayer = $success
 @onready var rocket_audio: AudioStreamPlayer3D = $RocketAudio
@@ -12,14 +13,28 @@ class_name Player
 @onready var explosion_particles: GPUParticles3D = $ExplosionParticles
 @onready var success_particles: GPUParticles3D = $SuccessParticles
 
+
+
+
+var ui : CanvasLayer
+
+
+var fuel : int:
+	set(new_fuel):
+		fuel = new_fuel
+		ui.update_fuel(new_fuel)
+
 var transitioning := false
 
 func _ready() -> void:
-	pass
+	ui = get_tree().get_first_node_in_group("ui")
+	fuel = starting_fuel
+	
 	
 func _process(delta: float) -> void:
 	if not transitioning:
-		if Input.is_action_pressed("boost"):
+		if Input.is_action_pressed("boost") and fuel > 0:
+			fuel -= 0.5
 			apply_central_force(basis.y * delta * thrust)
 			main_booster.emitting = true
 			if not rocket_audio.is_playing():
